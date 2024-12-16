4f4f434bf26b623837b342d132dc8202bbe69906
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-16 18:18:29 -0300

    login and logout logic entered

--- Bookstore/views.py
+++ Bookstore/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return HttpResponse("Teste Home")
+    if request.method == "POST":
+        username = request.POST["usuario"]
+        password = request.POST["senha"]
 
-def sobre(request):
-    return HttpResponse("Teste Sobre")
+        # Autenticação
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login efetuado com sucesso!")
+            return redirect("home")
+        else:
+            messages.error(request, "Erro na autenticação! Tente novamente!")
+            return redirect("home")
+    else:
+        return render(request, "home.html")
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout efetuado com sucesso!")
+    return redirect("home")
+
+
+def register_user(request):
+    return render(request, "register.html")
+
+
