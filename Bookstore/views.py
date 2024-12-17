f6b1e29a98e13c14ee1b94c39078b80557872b53
Robert Rudi <robertcortezrudi@gmail.com>
2024-12-17 00:32:12 -0300

    Register function created

--- Bookstore/views.py
+++ Bookstore/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     if request.method == "POST":
@@ -33,6 +34,17 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, "register.html")
-
-
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Autenticação e login
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, "Conta criada e login efetuado com sucesso!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "register.html", {"form": form})
